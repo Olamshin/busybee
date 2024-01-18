2f315850280c9a435ee2a4e47c54b2de4d2548fe
Olamide Kolawole <okolawole@ebsco.com>
2024-01-18 09:43:58 -0600

    fixed building of standalone executable

--- build.py
+++ build.py
@@ -2,8 +2,9 @@ import PyInstaller.__main__
 
 
 PyInstaller.__main__.run([
-    'src/cli.py',  # Replace with the name of your Python script
-    '--name=busybee',   # Set the desired name of the executable
+    'busybee/__main__.py',
+    '--onefile',
+    '--name=busybee',
     '--add-data=config.yml:.',
     '--noconfirm'
 ])
--- busybee/__main__.py
+++ busybee/__main__.py
@@ -1,4 +1,4 @@
-from .cli import BusyBeeCli
+from busybee.cli import BusyBeeCli
 import sys
 
 if __name__ == "__main__":
